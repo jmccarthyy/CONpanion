1338be30fcc090227d6b6c2e862d64b1a521c7f8
“jake” <mccarthyy.j@hotmail.com>
2024-02-06 15:57:27 +0000

    Initial Commit

---
+++ CONpanion/CONpanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CONpanionApp.swift
+//  CONpanion
+//
+//  Created by jake mccarthy on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CONpanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
