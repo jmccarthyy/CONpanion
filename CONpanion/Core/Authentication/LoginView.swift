e617fe62d58902c9b8d719d6b0a7eead70522259
“jake” <mccarthyy.j@hotmail.com>
2024-02-07 14:53:22 +0000

    Edited appearance of login page

--- CONpanion/Core/Authentication/LoginView.swift
+++ CONpanion/Core/Authentication/LoginView.swift
@@ -12,28 +12,36 @@ struct LoginView: View {
     @State private var password = ""
     var body: some View {
         NavigationStack{
-            ZStack{
+            VStack{
+                //background
                 RoundedRectangle(cornerRadius: 10, style: .continuous)
                     .foregroundStyle(.linearGradient(colors: [.customGradientColour, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
                     .frame(width:1000, height:400)
                     .rotationEffect(.degrees(135))
-                    .offset(y: -350)
+                    .offset(y: -150)
                 
-                VStack(spacing: 20) {
+                VStack(spacing: 24) {
+                    //text
                     Text("Your CONpanion Missed You")
                         .foregroundColor(.white)
-                        .font(.system(size: 28, weight: .bold, design: .rounded))
-                        .lineLimit(2)
-                        .offset( y: -250)
+                        .font(.system(size: 40, weight: .bold, design: .rounded))
+                        .lineLimit(3)
+                        .offset( y: -350)
                     
                     Text("Log in:")
                         .foregroundColor(.white)
                         .font(.system(size: 20, weight: .bold, design: .rounded))
-                        .offset(x: -150, y: -210)
+                        .offset(x: -150, y: -300)
                     
                     // form fields
+                    InputView(text: $email, title: "Email Address", placeholder: "name@email.com")
+                        .foregroundColor(.white)
+                        .offset(x: 15, y: -300)
+                        
                     
                     // sign in button
+                    
+                    Spacer()
                     
                     //registration link
                     
