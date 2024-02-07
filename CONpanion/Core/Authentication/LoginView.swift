3a26138e3e24997eebdb0fdb200e768944ecb637
“jake” <mccarthyy.j@hotmail.com>
2024-02-07 14:17:54 +0000

    created state properties for email and password

--- CONpanion/Core/Authentication/LoginView.swift
+++ CONpanion/Core/Authentication/LoginView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
         NavigationStack{
             ZStack{
