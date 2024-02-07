4a62cd6364bdc0d2146c9fbbe526112392bb4e27
“jake” <mccarthyy.j@hotmail.com>
2024-02-07 16:39:45 +0000

    created ForgottenPasswordView page for 'forgotten password' link on sign in page

---
+++ CONpanion/Core/Authentication/ForgottenPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgottenPasswordView.swift
+//  CONpanion
+//
+//  Created by jake mccarthy on 07/02/2024.
+//
+
+import SwiftUI
+
+struct ForgottenPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgottenPasswordView()
+}
