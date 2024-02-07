fdd89a96d0afbb25229ac85ed9eaf4ba7c5032f7
“jake” <mccarthyy.j@hotmail.com>
2024-02-07 16:48:30 +0000

    Added ProfileView file for user profile page

---
+++ CONpanion/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  CONpanion
+//
+//  Created by jake mccarthy on 07/02/2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
