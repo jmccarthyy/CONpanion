b3efa5da19315dedb3b34b73187e4d31a2f3b744
“jake” <mccarthyy.j@hotmail.com>
2024-02-07 15:25:05 +0000

    added password secure field to page, improved appearance, improved code quality

--- CONpanion/Core/Authentication/LoginView.swift
+++ CONpanion/Core/Authentication/LoginView.swift
@@ -18,34 +18,75 @@ struct LoginView: View {
                     .foregroundStyle(.linearGradient(colors: [.customGradientColour, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
                     .frame(width:1000, height:400)
                     .rotationEffect(.degrees(135))
-                    .offset(y: -150)
+                    .offset(y: -100)
                 
                 VStack(spacing: 24) {
                     //text
                     Text("Your CONpanion Missed You")
                         .foregroundColor(.white)
-                        .font(.system(size: 40, weight: .bold, design: .rounded))
-                        .lineLimit(3)
-                        .offset( y: -350)
+                        .font(.system(size: 35, weight: .bold, design: .rounded))
+                        .lineLimit(2)
+                        .offset(y: -300)
                     
                     Text("Log in:")
                         .foregroundColor(.white)
                         .font(.system(size: 20, weight: .bold, design: .rounded))
-                        .offset(x: -150, y: -300)
+                        .offset(x: -150, y: -250)
                     
                     // form fields
-                    InputView(text: $email, title: "Email Address", placeholder: "name@email.com")
+                    InputView(text: $email,
+                              title: "Email Address:",
+                              placeholder: "name@email.com")
+                        .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
                         .foregroundColor(.white)
-                        .offset(x: 15, y: -300)
-                        
+                        .offset(y: -250)
+                        .padding(.horizontal)
+                    
+                    InputView(text: $password,
+                              title: "Password:",
+                              placeholder: "Enter your password",
+                              isSecureField: true)
+                        .foregroundColor(.white)
+                        .offset(y: -250)
+                        .padding(.horizontal)
+                    
+                    //forgotten password link
                     
                     // sign in button
+                    Button{
+                        print("Log user in...")
+                    } label: {
+                        HStack{
+                            Text("SIGN IN")
+                                .fontWeight(.semibold)
+                            Image(systemName:"arrow.right")
+                        }
+                        .foregroundColor(.black)
+                        .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                    }
+                    .background(Color(.white))
+                    .cornerRadius(10)
+                    .padding(.top, 24)
+                    .offset(y: -250)
                     
                     Spacer()
                     
                     //registration link
+                    NavigationLink {
+                        
+                    } label: {
+                        HStack (spacing: 4){
+                            Text("Not yet been introduced?")
+                            Text("Sign Up")
+                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                .font(.system(size: 14))
+                        }
+                        .foregroundColor(.white)
+                    }
+                    .offset(y: -20)
                     
-                    //forgotten password link
+
+                   
                 }
             }
             .containerRelativeFrame([.horizontal, .vertical])
