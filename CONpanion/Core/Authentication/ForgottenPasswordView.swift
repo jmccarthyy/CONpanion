fc42a2b0b0fa4db16d86edffb218756ceb8bf446
“jake” <mccarthyy.j@hotmail.com>
2024-02-07 16:46:36 +0000

    Added UI background colours and link back to login page

--- CONpanion/Core/Authentication/ForgottenPasswordView.swift
+++ CONpanion/Core/Authentication/ForgottenPasswordView.swift
@@ -8,8 +8,32 @@
 import SwiftUI
 
 struct ForgottenPasswordView: View {
+    @Environment (\.dismiss) var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            //background
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                .foregroundStyle(.linearGradient(colors: [.customGradientColour, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width:1000, height:400)
+                .rotationEffect(.degrees(135))
+                .offset(y: -350)
+            
+            
+            
+            Button {
+                dismiss()
+            } label: {
+                HStack (spacing: 4){
+                    Text("I've remembered my password")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .font(.system(size: 14))
+                }
+                .foregroundColor(.white)
+            }
+            .offset(y: -100)
+        }
+        .containerRelativeFrame([.horizontal, .vertical])
+        .background(Color(.black))
     }
 }
 
