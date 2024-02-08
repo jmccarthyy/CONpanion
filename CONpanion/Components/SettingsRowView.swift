d6dcb82f0bc7e1ecc49479f0e84971f8e8e47b58
“jake” <mccarthyy.j@hotmail.com>
2024-02-08 13:54:04 +0000

    Created new component file for the UI rows on the profile page

---
+++ CONpanion/Components/SettingsRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsRowView.swift
+//  CONpanion
+//
+//  Created by jake mccarthy on 08/02/2024.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsRowView()
+}
