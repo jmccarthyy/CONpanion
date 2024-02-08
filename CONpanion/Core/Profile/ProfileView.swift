e54754f91c826437d667d33b916e2bc682a0f654
“jake” <mccarthyy.j@hotmail.com>
2024-02-08 13:31:05 +0000

    Created sections for profile settings

--- CONpanion/Core/Profile/ProfileView.swift
+++ CONpanion/Core/Profile/ProfileView.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            Section{
+                
+            }
+            
+            Section("General"){
+                
+            }
+            
+            Section("Accessibility"){
+                
+            }
+            
+            Section("Account"){
+                
+            }
+            
+        }
     }
 }
 
