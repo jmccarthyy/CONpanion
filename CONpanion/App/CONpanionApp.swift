e8d839b4585dca97bb1cdb7e9e6643eef3d45d2e
“jake” <mccarthyy.j@hotmail.com>
2024-02-15 13:37:59 +0000

    Configured firebase in main file

--- CONpanion/App/CONpanionApp.swift
+++ CONpanion/App/CONpanionApp.swift
@@ -6,10 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct CONpanionApp: App {
     @StateObject var viewModel = AuthViewModel()
+    
+    init() {
+        FirebaseApp.configure()
+    }
     
     var body: some Scene {
         WindowGroup {
