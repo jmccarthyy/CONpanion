eb1b6febd992f46e9d7acac9721169681697af40
“jake” <mccarthyy.j@hotmail.com>
2024-02-15 14:24:54 +0000

    Removed mock user details from profile view and replaced them with the details of the current user

--- CONpanion/Core/Profile/ProfileView.swift
+++ CONpanion/Core/Profile/ProfileView.swift
@@ -8,118 +8,122 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    
     var body: some View {
-        List{
-            Section{
-                HStack {
-                    Text(User.MOCK_USER.initials)
-                        .font(.system(size: 28))
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(width:72, height: 72)
-                        .background(Color(.systemGray3))
-                        .clipShape(Circle())
+        if let user = viewModel.currentUser {
+            List{
+                Section{
+                    HStack {
+                        Text(user.initials)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(width:72, height: 72)
+                            .background(Color(.systemGray3))
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(user.fullName)
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                                .padding(.top, 4)
+                            
+                            Text(user.email)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                            
+                        }
+                    }
                     
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(User.MOCK_USER.fullName)
-                            .font(.subheadline)
-                            .fontWeight(.bold)
-                            .padding(.top, 4)
+                }
+                
+                Section("General"){
+                    HStack{
+                    SettingsRowView(imageName: "gear",
+                                    title: "Version",
+                                    tintColor: Color(.systemGray))
+                    
+                    
+                    Spacer()
+                    
+                    Text("Alpha")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    
+                    }
+                }
+                
+                Section("Accessibility"){
+                    
+                    HStack{
+                    Button {
+                        print("Accessibility selected...")
+                    } label: {
+                        SettingsRowView(imageName: "accessibility",
+                                        title: "Accessibility",
+                                        tintColor: Color(.systemGray))
+                    }
                         
-                        Text(User.MOCK_USER.email)
-                            .font(.footnote)
-                            .foregroundColor(.gray)
+                    Spacer()
                         
+                    Image(systemName: "chevron.right")
+                        .imageScale(.small)
+                        .foregroundColor(Color(.systemGray))
+                    
+                    }
+                    
+                    HStack{
+                    Button {
+                        print("Font size selected...")
+                    } label: {
+                    SettingsRowView(imageName: "textformat.size",
+                                    title: "Font Size",
+                                    tintColor: Color(.systemGray))
+                    }
+                    
+                    Spacer()
+                        
+                    Image(systemName: "chevron.right")
+                        .imageScale(.small)
+                        .foregroundColor(Color(.systemGray))
+                    
+                    }
+                    
+                    HStack{
+                    Button {
+                        print("App appearance selected...")
+                    } label: {
+                        SettingsRowView(imageName: "eye",
+                                        title: "App Appearance",
+                                        tintColor: Color(.systemGray))
+                    }
+                    
+                    Spacer()
+                        
+                    Image(systemName: "chevron.right")
+                        .imageScale(.small)
+                        .foregroundColor(Color(.systemGray))
+                    
+                    }
+                    
+                }
+                
+                Section("Account"){
+                    Button {
+                        print("Sign out...")
+                    } label: {
+                        SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: Color(.red))
+                    }
+                    
+                    Button {
+                        print("Delete account...")
+                    } label: {
+                        SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: Color(.red))
                     }
                 }
                 
             }
-            
-            Section("General"){
-                HStack{
-                SettingsRowView(imageName: "gear",
-                                title: "Version",
-                                tintColor: Color(.systemGray))
-                
-                
-                Spacer()
-                
-                Text("Alpha")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                
-                }
-            }
-            
-            Section("Accessibility"){
-                
-                HStack{
-                Button {
-                    print("Accessibility selected...")
-                } label: {
-                    SettingsRowView(imageName: "accessibility",
-                                    title: "Accessibility",
-                                    tintColor: Color(.systemGray))
-                }
-                    
-                Spacer()
-                    
-                Image(systemName: "chevron.right")
-                    .imageScale(.small)
-                    .foregroundColor(Color(.systemGray))
-                
-                }
-                
-                HStack{
-                Button {
-                    print("Font size selected...")
-                } label: {
-                SettingsRowView(imageName: "textformat.size",
-                                title: "Font Size",
-                                tintColor: Color(.systemGray))
-                }
-                
-                Spacer()
-                    
-                Image(systemName: "chevron.right")
-                    .imageScale(.small)
-                    .foregroundColor(Color(.systemGray))
-                
-                }
-                
-                HStack{
-                Button {
-                    print("App appearance selected...")
-                } label: {
-                    SettingsRowView(imageName: "eye",
-                                    title: "App Appearance",
-                                    tintColor: Color(.systemGray))
-                }
-                
-                Spacer()
-                    
-                Image(systemName: "chevron.right")
-                    .imageScale(.small)
-                    .foregroundColor(Color(.systemGray))
-                
-                }
-                
-            }
-            
-            Section("Account"){
-                Button {
-                    print("Sign out...")
-                } label: {
-                    SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: Color(.red))
-                }
-                
-                Button {
-                    print("Delete account...")
-                } label: {
-                    SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: Color(.red))
-                }
-            }
-            
         }
     }
 }
