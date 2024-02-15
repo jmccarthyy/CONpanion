6cceabe0ed58a873870f7b7308206e4c16571a5f
“jake” <mccarthyy.j@hotmail.com>
2024-02-15 13:37:34 +0000

    for the createUser function, an asynchronous function that can potentially throw an error was added that creates user using Firebase code and encodes our user object to it before uploading data to firestore

--- CONpanion/Core/Authentication/ViewModel/AuthViewModel.swift
+++ CONpanion/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Firebase
+import FirebaseFirestoreSwift
 
 class AuthViewModel: ObservableObject {
     @Published var userSession: FirebaseAuth.User?
@@ -21,7 +22,15 @@ class AuthViewModel: ObservableObject {
     }
     
     func createUser(withEmail email: String, password: String, fullName: String) async throws {
-        print("Create User...")
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            let user = User(id: result.user.uid, fullName: fullName, email: email)
+            let encodedUser = try Firestore.Encoder().encode(user)
+            try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+        } catch {
+            print("Failed to create user with error \(error.localizedDescription)")
+        }
     }
     
     func signOut() {
