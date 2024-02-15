8550b2bb99bf47859f6ebc6dc374ec745e75565e
“jake” <mccarthyy.j@hotmail.com>
2024-02-15 14:40:51 +0000

    Replaced placeholder print text with sign out function

--- CONpanion/Core/Profile/ProfileView.swift
+++ CONpanion/Core/Profile/ProfileView.swift
@@ -111,13 +111,13 @@ struct ProfileView: View {
                 
                 Section("Account"){
                     Button {
-                        print("Sign out...")
+                        viewModel.signOut()
                     } label: {
                         SettingsRowView(imageName: "arrow.left.circle.fill", title: "Sign Out", tintColor: Color(.red))
                     }
                     
                     Button {
-                        print("Delete account...")
+                        print("Delete Account...")
                     } label: {
                         SettingsRowView(imageName: "xmark.circle.fill", title: "Delete Account", tintColor: Color(.red))
                     }
