bf5b87d9cd8cd68b5a861d0ca6b79172fc9d0b08
jmccarthyy <mccarthyy.j@hotmail.com>
2024-02-23 13:48:07 +0000

    Created file for Navigation Bar

---
+++ CONpanion/Components/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  CONpanion
+//
+//  Created by jake mccarthy on 23/02/2024.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
