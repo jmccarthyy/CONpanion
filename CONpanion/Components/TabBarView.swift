b3c59236e8ad795748bb083c1dc0c11ccc2d0e37
jmccarthyy <mccarthyy.j@hotmail.com>
2024-02-23 13:52:13 +0000

    Created empty tab to see what current tab is

--- CONpanion/Components/TabBarView.swift
+++ CONpanion/Components/TabBarView.swift
@@ -7,6 +7,10 @@
 
 import SwiftUI
 
+enum Tab: String, CaseIterable {
+    
+}
+
 struct TabBarView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
